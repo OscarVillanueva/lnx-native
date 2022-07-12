a01126ad82a5d8927a11ed7a7b418c63fa9910d3
Javier Villanueva <javier.villanueva@rookmotion.com>
2022-07-12 16:33:36 -0500

    :bug: fixing typigs

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -11,68 +11,83 @@ type Theme = {
   label: any,
   title: any,
   background: any
-} | {}
+}
 
 export const useTheme = (): Theme => {
   // Sacamos el tema que esta usando el sistema
   const scheme = useColorScheme();
 
   // State para el tema
-  const [theme, setTheme] = useState<Theme>({});
+  const [theme, setTheme] = useState<Theme>(StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+
+    content: {
+      flex: 1,
+      marginHorizontal: '2.5%',
+    },
+
+    button: {
+      backgroundColor: '#ffda00',
+    },
+
+    textButton: {
+      textTransform: 'uppercase',
+      fontWeight: 'bold',
+      color: '#000',
+    },
+
+    img: {
+      width: '100%',
+      height: 300,
+    },
+
+    price: {
+      marginVertical: 20,
+      textAlign: 'center',
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: scheme === 'light' ? '#1a202c' : '#edf2f7',
+    },
+
+    label: {
+      color: scheme === 'light' ? '#1a202c' : '#edf2f7',
+      fontSize: 18,
+    },
+
+    title: {
+      color: scheme === 'light' ? '#1a202c' : '#edf2f7',
+      textAlign: 'center',
+      marginTop: 40,
+      marginBottom: 20,
+      fontSize: 30,
+    },
+
+    background: {
+      backgroundColor: scheme === 'light' ? '#f7fafc' : '#718096',
+    },
+  }));
 
   useEffect(() => {
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-      },
-
-      content: {
-        flex: 1,
-        marginHorizontal: '2.5%',
-      },
-
-      button: {
-        backgroundColor: '#ffda00',
-      },
-
-      textButton: {
-        textTransform: 'uppercase',
-        fontWeight: 'bold',
-        color: '#000',
-      },
-
-      img: {
-        width: '100%',
-        height: 300,
-      },
-
-      price: {
-        marginVertical: 20,
-        textAlign: 'center',
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: scheme === 'light' ? '#1a202c' : '#edf2f7',
-      },
-
-      label: {
-        color: scheme === 'light' ? '#1a202c' : '#edf2f7',
-        fontSize: 18,
-      },
-
-      title: {
-        color: scheme === 'light' ? '#1a202c' : '#edf2f7',
-        textAlign: 'center',
-        marginTop: 40,
-        marginBottom: 20,
-        fontSize: 30,
-      },
-
+    setTheme(styles => ({
+      ...styles,
       background: {
         backgroundColor: scheme === 'light' ? '#f7fafc' : '#718096',
       },
-    });
-
-    setTheme(styles);
+      title: {
+        ...styles.title,
+        color: scheme === 'light' ? '#1a202c' : '#edf2f7',
+      },
+      label: {
+        ...styles.label,
+        color: scheme === 'light' ? '#1a202c' : '#edf2f7',
+      },
+      price: {
+        ...styles.price,
+        color: scheme === 'light' ? '#1a202c' : '#edf2f7',
+      }
+    }));
   }, [scheme]);
 
   return theme;
